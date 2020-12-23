d9e2ae20d47e7f45ee8dcd62c1fd8e7d3f9924bb
meroycha <aditya.roychaudhury@gmail.com>
2020-12-23 15:46:44 +0530

    3nd commit

--- src/Ch02.scala
+++ src/Ch02.scala
@@ -1,89 +1,38 @@
-import scala.collection.mutable
+import java.math.BigInteger
 
 object Ch02 extends App {
-  val greetStrings = new Array[String](3)
+  var capital = Map("India" -> "New Delhi", "France" -> "Paris")
+  capital += ("Australia" -> "Canberra")
 
-  greetStrings(0) = "Hello"
-  greetStrings(1) = ", "
-  greetStrings(2) = "world!\n"
+  //println(capital("France"))
 
-//  for (i <- 0 to 2)
-//    print(greetStrings(i))
+  def factorial(x: BigInteger): BigInteger =
+    if (x == BigInteger.ZERO)
+      BigInteger.ONE
+    else
+      x.multiply(factorial(x.subtract(BigInteger.ONE)))
 
-  /*
-  List
-   */
+  def max(x: Int, y: Int): Int = {
+    if (x > y) x
+    else y
+  }
 
-  val oneTwo = List(1, 2)
-  val threeFour = List(3, 4)
-  val oneTwoThreeFour = oneTwo ::: threeFour
+  def max2(x: Int, y: Int): Int = if (x > y) x else y
 
-  //println("" + oneTwo + " and " + threeFour + " were not mutated.")
-  //println("This, " + oneTwoThreeFour + " is a new list.")
+  //println(factorial(BigInteger.valueOf(6)))
 
-  // Some 'List' methods and usages
+  val name = "Radhika Ghosh"
+  //println(name.exists(_.isWhitespace))
 
-  var list = List("Cool", "tools", "rule")
-  //println(list)
+  //println(max(10, 12))
+  //println(max2(12, 10))
 
-  val thrill = "Will" :: "fill" :: "until" :: Nil
-//  println(thrill)
-//  println("The Length of 'thrill' is = " + thrill.size)
-//  println(thrill.drop(2))
-//  println(thrill.dropRight(2))
-//  println(thrill.contains("until"))
-//  println(thrill.filter(s => s.length == 4))
-//  println(thrill.forall(s => s.endsWith("l")))
-//  thrill.foreach(s=>print(s))
-
-//  println(thrill.head)
-//  println(thrill.init)
-//  println(thrill.last)
-
-//  println(thrill.map(s => s + "y"))
-//  println(thrill.mkString(", "))
-//  println(thrill.reverse)
-
-  /****************************
-   * Use Tuples
-   */
-  val tuple = (36, "India", "Australia", "Hazelwood", "Rahane", "Kohli")
-//  println(tuple._3)
-//  println(tuple._6)
-
-  /****************************
-   * || Class Hierarchy for Scala sets ||
-   * scala.collection.Set<<trait>>
-   *   - scala.collection.immutable.Set<<trait>>
-   *     - scala.collection.immutable.HashSet
-   *   - scala.collection.mutable.Set<<trait>>
-   *     - scala.collection.mutable.HashSet
-   *
-   */
-  var jetSet = Set("Boeing", "Airbus")
-  jetSet += "Lear"
-  println(jetSet)
-  println(jetSet.contains("Cessna"))
-
-  import scala.collection.immutable.HashSet
-  val hashSet = HashSet("Tomatoes", "Chilies")
-  println(hashSet + "Coriander")
-
-  /****************************
-   * || Class Hierarchy for Scala maps ||
-   * scala.collection.Map<<trait>>
-   *   - scala.collection.immutable.Map<<trait>>
-   *     - scala.collection.immutable.HashMap
-   *   - scala.collection.mutable.Map<<trait>>
-   *     - scala.collection.mutable.HashMap
-   *
-   */
-
-  import scala.collection.mutable.Map
-  val treasureMap = mutable.Map[Int, String]()
-  treasureMap += (1 -> "Go to island.")
-  treasureMap += (2 -> "Find big X on ground.")
-  treasureMap += (3 -> "Dig.")
-  println(treasureMap(2))
-
+  var i = 0
+  while (i < 5) {
+    if (i != 0)
+      print(" ")
+    print(i)
+    i += 1
+  }
 }
+
